b8ce55b305e54dbe01986f7aa993584469f0c55f
Alex <alexhaigh23@outlook.com>
2021-05-24 14:56:47 +0100

    completed sticky header functionaility

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -24,13 +24,11 @@ class RevealOnScroll {
     events() {
         window.addEventListener('scroll', this.scrollThrottle);
         window.addEventListener('resize', debounce(() => {
-            console.log('resize just ran');
             this.browserHeight = window.innerHeight;
         }, 333))
     }
 
     calcCaller() {
-        console.log('scroll function ran');
             this.itemsToReveal.forEach(el => {
                 if (el.isRevealed == false) {
                     this.calculateIfScrolledTo(el);
@@ -40,7 +38,7 @@ class RevealOnScroll {
 
     calculateIfScrolledTo(el) {
         if (window.scrollY + this.browserHeight > el.offsetTop) {
-            console.log('element was calculated');
+            
             let scrollPercent = (el.getBoundingClientRect().top / this.browserHeight) * 100;
 
             if (scrollPercent < this.thresholdPercent) {
